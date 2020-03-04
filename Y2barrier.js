39df2e69d719e1be5efa7486700c579528850750
JajileSalami <56744453+JajileSalami@users.noreply.github.com>
2020-03-04 13:54:39 -1000

    updated files to have an included barrier
    *barrier has no hitbox

---
+++ Y2barrier.js
@@ -0,0 +1,30 @@
+
+
+
+function barrier() {
+  this.h = window.innerHeight;
+  this.randh = 0;
+  this.x1 = w-20;
+  this.y1 = 100;
+  this.gravity = 0.5;
+  this.velocity = -10;
+  
+  
+  
+  this.show = function() {
+    fill(0, 0,255);
+    rect(this.x1, this.y1, 20, 200);
+  };
+  
+  this.update = function() {
+    this.x1 += this.velocity;
+    if (this.x1 < 0) {
+      this.y1 = random(h);
+    }
+    //randh = (h - (h * random(h)));
+    if (barrier.x1 < 0) {
+      barrier.x1 = w-10;
+    }
+  };
+
+}
--- Y2jumper.js
+++ Y2jumper.js
@@ -1,4 +1,47 @@
+
+
+
 function jumper() {
+  // this.#operation = value;
+  this.x = w/2;
+  this.y = h/2;
+  this.gravity = 0.5;//gravity
+  this.lift = -10;//jump height
+  this.velocity = 0;//speed of gravity
+  
+  
+  //function on screen to put values of character will look like
+  this.show = function() {
+    fill(255, 0, 0);
+    ellipse(this.x, this.y, 50,50);
+  };
+  
+  //build function to take intial velocity and change lift/jump
+  this.up = function() {
+    this.velocity += this.lift;
+  };
+  
+  //builds function to update character
+  this.update = function() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity * 0.9; //air resistance
+    
+    //IF character hits groubd, movement in y direction will have jumper from leaving ground
+    if (this.y > h-25) {
+      this.y = h-25;
+      this.velocity = 0;
+    }
+    
+    //stops character from leaving ceiling of screen
+    if (this.y < 25) {
+      this.y = 25;
+      this.velocity = 0;
+    }
+  };
+}
+
+/*function jumper() {
   this.x = w/2;
   this.y = h/2;
   this.gravity = 0.5; // the force of gravity
@@ -31,3 +74,40 @@ function jumper() {
     }
   }
 }
+
+
+
+function barrier() {
+    this.x = w;
+    this.y = 100;
+    let rand = random(w);
+    //Barrier appears
+    this.show = function() {
+        fill("Blue");
+        rect(this.x,this.y,50,200);
+    }
+    //Barrier moves
+    this.move = function() {
+        if (this.x < 0) {
+            this.x += -10;
+        }
+    }
+}
+
+
+function barrier() {
+  this.x = w/2;
+  this.y = h/2;
+  this.push = -10;
+  this.horizontal = 0.5;
+  let rand = random(0, 100);
+  
+  this.shape = function() {
+    fill(0,0,255);
+    rect(x, rand, 50, 200);
+  }
+  this.refresh = function() {
+    this.x += this.push;
+  }
+}
+*/
